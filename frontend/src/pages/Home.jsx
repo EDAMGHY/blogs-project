0caf5cf2570e86a204655311210339e8cc1cd34f
Abdellah DAMRI <damriabdellah@gmail.com>
2023-12-26 21:29:17 +0100

    Add: testing the colors in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 function Home() {
-  return <div>Homepgae</div>;
+  return (
+    <div className='p-5'>
+      <div className='p-4 bg-secondary text-primary'>Home</div>
+      <div className='p-2 bg-info'>Info</div>
+      <div className='p-2 bg-success'>success</div>
+      <div className='p-2 bg-error'>error</div>
+      <div className='p-2 bg-warning'>warning</div>
+    </div>
+  );
 }
 
 export default Home;
