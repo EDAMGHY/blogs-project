84c44c80e48ace6d60c0303911a8c067895be603
Abdellah DAMRI <damriabdellah@gmail.com>
2023-12-31 13:49:31 +0100

    Add: adding the header for the dashboard

--- frontend/src/components/layout/index.js
+++ frontend/src/components/layout/index.js
@@ -1,4 +1,6 @@
 export { default as DashboardLayout } from "./DashboardLayout";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
+export { default as Sidebar } from "./Sidebar";
+export { default as DashboardHeader } from "./DashboardHeader";
 export { default as MainLayout } from "./MainLayout";
